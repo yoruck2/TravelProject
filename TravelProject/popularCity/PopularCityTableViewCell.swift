6d77c964049742d48a6e540f692e9cc931eb973a
yoruck2 <yoruck2@gmail.com>
2024-05-28 02:48:35 +0900

    PopularCityTableViewCell 추가

--- TravelProject/popularCity/PopularCityTableViewCell.swift
+++ TravelProject/popularCity/PopularCityTableViewCell.swift
@@ -6,18 +6,41 @@
 //
 
 import UIKit
+import Kingfisher
 
 class PopularCityTableViewCell: UITableViewCell {
 
+    @IBOutlet var spotNameLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var saveLabel: UILabel!
+    @IBOutlet var cellImage: UIImageView!
+    
+    @IBOutlet var gradeLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    func configureLayout() {
+        spotNameLabel?.font = .boldSystemFont(ofSize: 17)
+        descriptionLabel?.font = .systemFont(ofSize: 13)
+        descriptionLabel?.textColor = .darkGray
+        saveLabel?.font = .boldSystemFont(ofSize: 13)
+        saveLabel?.textColor = .gray
+        cellImage.layer.cornerRadius = 10
 
-        // Configure the view for the selected state
     }
     
+    func configureCell(data: Travel) {
+        spotNameLabel?.text = data.title
+        descriptionLabel.text = data.description
+        gradeLabel.text = String(format:"%.1f", data.grade ?? "")
+
+        
+        let url = URL(string: data.travel_image ?? "")
+        cellImage.kf.setImage(with: url)
+        
+        
+    }
 }
