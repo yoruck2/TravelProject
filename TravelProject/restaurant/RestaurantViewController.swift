2f4b7f4e00de0b7e3bf735f2b651f9c624bf51c4
yoruck2 <yoruck2@gmail.com>
2024-05-28 02:49:58 +0900

    RestaurantViewController 추가

--- TravelProject/restaurant/RestaurantViewController.swift
+++ TravelProject/restaurant/RestaurantViewController.swift
@@ -7,23 +7,61 @@
 
 import UIKit
 
-class RestaurantViewController: UIViewController {
+class RestaurantViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
+    let restaurantList = RestaurantList().restaurantArray
+    var filteredList: [Restaurant] = []
+    
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var restaurantTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let left = UIBarButtonItem(title: "한식만", style: .plain, target: self, action: #selector(leftBarButtonClicked))
+        navigationItem.leftBarButtonItem = left
+        
+        restaurantTableView.rowHeight = 230
+        
+        searchBar.delegate = self
+        restaurantTableView.delegate = self
+        restaurantTableView.dataSource = self
+        
+        let nib = UINib(nibName: "RestaurantTableViewCell", bundle: nil)
+        restaurantTableView.register(nib, forCellReuseIdentifier: "RestaurantTableViewCell")
+        
+        filteredList = restaurantList
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        
+//        var searchList: [Restaurant] = []
+//        
+//        for item in restaurantList {
+//            if item.name.contains(searchBar.text!) || item.category.contains(searchBar.text!) {
+//                searchList.append(item)
+//            }
+//        }
+//    }
+    
+    @objc
+    func leftBarButtonClicked() {
+        let filteredList = restaurantList.filter { $0.category == "한식" }
+        restaurantTableView.reloadData()
+        print(filteredList)
+        print(filteredList.count)
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        filteredList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantTableViewCell", for: indexPath) as! RestaurantTableViewCell
+        let data = filteredList[indexPath.row]
+        
+        cell.configureCell(data: data)
 
+        return cell
+    }
 }
