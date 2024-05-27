758f432c133dda9e23014c1bf6f566a1602d0a4e
yoruck2 <yoruck2@gmail.com>
2024-05-28 02:49:42 +0900

    RestaurantTablewViewCell 추가

--- TravelProject/restaurant/RestaurantTableViewCell.swift
+++ TravelProject/restaurant/RestaurantTableViewCell.swift
@@ -6,18 +6,58 @@
 //
 
 import UIKit
+import Kingfisher
 
 class RestaurantTableViewCell: UITableViewCell {
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
 
+    @IBOutlet var restaurantImage: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var contactLabel: UILabel!
+    @IBOutlet var adressLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    
+    @IBOutlet var categoryLabel: UIButton!
+    @IBOutlet var hearrButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        configureLayout()
+        
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func setUpImageView() {
+        
+        
     }
     
+    func configureLayout() {
+        nameLabel?.font = .boldSystemFont(ofSize: 17)
+        contactLabel?.font = .systemFont(ofSize: 13)
+        contactLabel?.textColor = .darkGray
+        adressLabel?.font = .boldSystemFont(ofSize: 13)
+        restaurantImage.layer.cornerRadius = 15
+        
+        categoryLabel.layer.cornerRadius = 16
+        categoryLabel.backgroundColor = #colorLiteral(red: 0.1098039216, green: 0.7294117647, blue: 0.9764705882, alpha: 1)
+        categoryLabel.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
+    func configureCell(data: Restaurant) {
+        nameLabel?.text = data.name
+        contactLabel.text = data.phoneNumber
+        adressLabel.text = data.address
+        priceLabel.text = data.price.formatted() + "원"
+        categoryLabel.setTitle(data.category, for: .normal)
+        
+        let url = URL(string: data.image)
+        restaurantImage.kf.setImage(with: url)
+        
+        
+    }
+
 }
