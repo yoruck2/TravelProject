b112cfd4cd1b61258f45c51154ba6f6ee424122e
yoruck2 <yoruck2@gmail.com>
2024-05-28 21:42:46 +0900

    travel 을 타입 프로퍼티로 변경

--- TravelProject/Model/TravelModel.swift
+++ TravelProject/Model/TravelModel.swift
@@ -17,7 +17,7 @@ struct Travel {
 }
 
 struct TravelInfo {
-    let travel: [Travel] = [
+    static let travel: [Travel] = [
         Travel(title: "하나우마 베이",
                description: "아름다운 자연을 감상할 수 있는 스노쿨링 명소",
                travel_image: "https://images.unsplash.com/photo-1539498508910-091b5e859b1d?q=80&w=3250&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
