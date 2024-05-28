b3d4ffb995ea778e150f0542074c9bc988b2b7f8
yoruck2 <yoruck2@gmail.com>
2024-05-28 21:44:29 +0900

    PopularCityViewController 추가

--- TravelProject/popularCity/PopularCityViewController.swift
+++ TravelProject/popularCity/PopularCityViewController.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class PopularCityViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    let popularCityList = TravelInfo().travel
+    
+    let popularCityList = TravelInfo.travel
     
     @IBOutlet var popularCityTableView: UITableView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureCell()
+    }
+    
+    func configureCell() {
         popularCityTableView.rowHeight = 150
-        
         popularCityTableView.delegate = self
         popularCityTableView.dataSource = self
-        
-        let nib = UINib(nibName: "PopularCityTableViewCell", bundle: nil)
-        popularCityTableView.register(nib, forCellReuseIdentifier: "PopularCityTableViewCell")
+        registerCell(id: "PopularCityTableViewCell")
+        registerCell(id: "AdTableViewCell")
+    }
     
+    func registerCell(id: String) {
+        let nib = UINib(nibName: id, bundle: nil)
+        popularCityTableView.register(nib, forCellReuseIdentifier: id)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -31,12 +37,17 @@ class PopularCityViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PopularCityTableViewCell", for: indexPath) as! PopularCityTableViewCell
+        
         let data = popularCityList[indexPath.row]
         
-        cell.configureCell(data: data)
-
-        return cell
+        if data.ad {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AdTableViewCell", for: indexPath) as! AdTableViewCell
+            cell.configureCell(data: data)
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PopularCityTableViewCell", for: indexPath) as! PopularCityTableViewCell
+            cell.configureCell(data: data)
+            return cell
+        }
     }
-
 }
