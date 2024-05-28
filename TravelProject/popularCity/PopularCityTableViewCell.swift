835774bea1e423178b215ac22fb74865484769c4
yoruck2 <yoruck2@gmail.com>
2024-05-28 21:44:10 +0900

    PopularCityTableViewCell 추가

--- TravelProject/popularCity/PopularCityTableViewCell.swift
+++ TravelProject/popularCity/PopularCityTableViewCell.swift
@@ -7,40 +7,60 @@
 
 import UIKit
 import Kingfisher
+import Cosmos
 
 class PopularCityTableViewCell: UITableViewCell {
+    
+//    static let identifier = "PopularCityTableViewCell"
 
     @IBOutlet var spotNameLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
     @IBOutlet var saveLabel: UILabel!
+    @IBOutlet var ratingLabel: CosmosView!
+    @IBOutlet var gradeLabel: UILabel!
+    
     @IBOutlet var cellImage: UIImageView!
     
-    @IBOutlet var gradeLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        configureLayout()
     }
 
     func configureLayout() {
         spotNameLabel?.font = .boldSystemFont(ofSize: 17)
-        descriptionLabel?.font = .systemFont(ofSize: 13)
+        descriptionLabel?.font = .systemFont(ofSize: 15)
         descriptionLabel?.textColor = .darkGray
         saveLabel?.font = .boldSystemFont(ofSize: 13)
         saveLabel?.textColor = .gray
         cellImage.layer.cornerRadius = 10
-
+        cellImage.contentMode = .scaleAspectFill
+        
+        ratingLabel.backgroundColor = .clear
+        ratingLabel.settings.updateOnTouch = false
+        ratingLabel.settings.fillMode = .half
+        ratingLabel.settings.starSize = 15
+        ratingLabel.settings.totalStars = 5
+        ratingLabel.settings.starMargin = 0
+        
+        
     }
     
     func configureCell(data: Travel) {
+        
+        guard let grade = data.grade, let save = data.save else {
+            return
+        }
+        
         spotNameLabel?.text = data.title
         descriptionLabel.text = data.description
-        gradeLabel.text = String(format:"%.1f", data.grade ?? "")
 
-        
         let url = URL(string: data.travel_image ?? "")
         cellImage.kf.setImage(with: url)
         
+        saveLabel.text = "･ ♥ \(save.formatThirdComma)"
+        ratingLabel.rating = grade
+        ratingLabel.text = "(\(grade))"
         
     }
 }
