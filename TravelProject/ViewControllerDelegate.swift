006496732aefc04d038140987308bcba98e1b872
yoruck2 <yoruck2@gmail.com>
2024-05-30 02:30:41 +0900

    데이터 전달을 위한 ViewControllerDelegate 프로토콜 추가

---
+++ TravelProject/ViewControllerDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  ViewControllerDelegate.swift
+//  TravelProject
+//
+//  Created by dopamint on 5/29/24.
+//
+
+import Foundation
+
+@objc
+protocol ViewControllerDelegate: AnyObject {
+    @objc optional func dismissViewController(data: String)
+    @objc optional func applyData(row: Int)
+}
+
