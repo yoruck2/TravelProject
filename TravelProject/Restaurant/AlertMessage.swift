59270cbbdc316523eb39dec2ab6eb08a7c48749d
yoruck2 <yoruck2@gmail.com>
2024-06-20 05:55:44 +0900

    Alert Message 추가

--- TravelProject/Restaurant/AlertMessage.swift
+++ TravelProject/Restaurant/AlertMessage.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+enum LocationServieAlertMessage {
+    case servieOff
+    case denied
+    
+    func showMessage() -> String {
+        switch self {
+        case .servieOff:
+            return "위치 서비스를 사용할 수 없습니다.\n기기의 설정에서 위치 서비스를 켜주세요."
+        case .denied:
+            return "위치 서비스를 사용할 수 없습니다.\n기기의 설정에서 앱의 위치 서비스 사용 권한을 허가 해주세요."
+        }
+        
+    }
+}
